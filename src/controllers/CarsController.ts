20768101ba9c62bd166b5f354fc7da5f76d9d711
Istvan <olivaistvan@gmail.com>
2022-10-10 12:16:24 -0300

    refactor: (Controllers) class CarsController

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -1,18 +1,4 @@
-import { Request, Response } from 'express';
-import { IService } from '../interfaces/IService';
 import { ICar } from '../interfaces/ICar';
+import Controller from './Controller';
 
-class CarController {
-  private _service: IService<ICar>;
-
-  constructor(service: IService<ICar>) {
-    this._service = service;
-  }
-
-  async create(req: Request, res: Response<ICar>) {
-    const newCar = await this._service.create(req.body);
-    return res.status(201).json(newCar);
-  }
-}
-
-export default CarController;
+export default class CarsController extends Controller<ICar> {}
