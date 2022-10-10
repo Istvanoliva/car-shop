4dc33d36cc2a57f79d650b942f66d9279b31ae56
Istvan <olivaistvan@gmail.com>
2022-10-10 15:13:16 -0300

    refactor: (routers) motorcycleRouter

--- src/routers/motorcycle.ts
+++ src/routers/motorcycle.ts
@@ -10,10 +10,12 @@ const motorcycleModel = new MotorcycleModel();
 const motorcycleService = new MotorcycleService(motorcycleModel, motorcycleZodSchema);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
+const motorcycle = '/motorcycles/:id';
+
 motorcycleRouter.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 motorcycleRouter.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
-motorcycleRouter.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
-motorcycleRouter.put('/motorcycles/:id', (req, res) => motorcycleController.update(req, res));
-motorcycleRouter.delete('/motorcycles/:id', (req, res) => motorcycleController.delete(req, res));
+motorcycleRouter.get(motorcycle, (req, res) => motorcycleController.readOne(req, res));
+motorcycleRouter.put(motorcycle, (req, res) => motorcycleController.update(req, res));
+motorcycleRouter.delete(motorcycle, (req, res) => motorcycleController.delete(req, res));
 
 export default motorcycleRouter;
