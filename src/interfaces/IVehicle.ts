ab2e62482c21c42c8a9689b9d7a714a856406e9f
Istvan <olivaistvan@gmail.com>
2022-10-10 14:53:38 -0300

    refactor: (Interfaces) IVehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,12 +1,5 @@
 import { z } from 'zod';
-
-const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
-});
+import vehicleZodSchema from '../zodSchemas/vehicleSchema';
 
 export type IVehicle = z.infer<typeof vehicleZodSchema>;
 
