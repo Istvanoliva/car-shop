7fb870a48ccd3f2816cf595c73f4a085e0d43e91
Istvan <olivaistvan@gmail.com>
2022-10-10 14:53:47 -0300

    refactor: (Interfaces) ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,10 +1,5 @@
 import { z } from 'zod';
-import { vehicleZodSchema } from './IVehicle';
-
-const carZodSchema = vehicleZodSchema.extend({
-  doorsQty: z.number().gte(2).lte(4),
-  seatsQty: z.number().gte(2).lte(7),
-});
+import carZodSchema from '../zodSchemas/carSchema';
 
 export type ICar = z.infer<typeof carZodSchema>;
 
