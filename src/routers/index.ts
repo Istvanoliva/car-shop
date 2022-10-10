ee94d61c9dc77823e56367fd2c37c0d5e5db56bb
Istvan <olivaistvan@gmail.com>
2022-10-10 14:56:55 -0300

    feat (routers) create motorcycleRouter

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import cars from './cars';
+import motorcycleRouter from './motorcycle';
 
 const router = Router();
 
 router.use(cars);
+router.use(motorcycleRouter);
 
 export default router;
---
+++ src/routers/motorcycle.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { motorcycleZodSchema } from '../interfaces/IMotorcycle';
+import MotorcycleController from '../controllers/MotorcycleController';
+import MotorcycleService from '../services/MotorcycleService';
+import MotorcycleModel from '../models/MotorcycleModel';
+
+const motorcycleRouter = Router();
+
+const motorcycleModel = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycleModel, motorcycleZodSchema);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+motorcycleRouter.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
+
+export default motorcycleRouter;
