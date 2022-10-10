8a75d311ab014a4fc4da1113db4203c13a3dcd17
Istvan <olivaistvan@gmail.com>
2022-10-10 01:20:45 -0300

    feat: (app) add routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import router from './routers';
+import errorMiddleware from './middlewares/errorMiddleware';
 
-const app = express(); 
+const app = express();
+
+app.use(router);
+
+app.use(errorMiddleware);
 
 export default app;
