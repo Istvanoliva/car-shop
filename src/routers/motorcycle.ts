a12055e6a40ee6dd7f02d9cc9ec29f35a04a31ce
Istvan <olivaistvan@gmail.com>
2022-10-10 15:06:49 -0300

    feat (motorcycleRouter) create GET /motorcycles/:id

--- src/routers/motorcycle.ts
+++ src/routers/motorcycle.ts
@@ -12,5 +12,6 @@ const motorcycleController = new MotorcycleController(motorcycleService);
 
 motorcycleRouter.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 motorcycleRouter.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
+motorcycleRouter.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
 
 export default motorcycleRouter;
