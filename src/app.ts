f7884be0568fa49b64884990be11beb1137214ee
Istvan <olivaistvan@gmail.com>
2022-10-10 12:02:59 -0300

    feat: (app) add new express configs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import router from './routers';
 import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
+
+app.use(express.json());
 
 app.use(router);
 
