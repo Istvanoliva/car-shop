f8e4503eef13fa0f5c80e1d5b45497b3b6219b97
Istvan <olivaistvan@gmail.com>
2022-10-10 01:20:16 -0300

    feat: (routers) create carsRouter

---
+++ src/routers/cars.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { carZodSchema } from '../interfaces/ICar';
+import CarsController from '../controllers/CarsController';
+import CarsModel from '../models/CarsModel';
+import CarsService from '../services/CarsService';
+
+const carsRouter = Router();
+
+const carModel = new CarsModel();
+const carService = new CarsService(carModel, carZodSchema);
+const carController = new CarsController(carService);
+
+carsRouter.post('/cars', carController.create);
+
+export default carsRouter;
